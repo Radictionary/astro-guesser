5dcab32701011f69547cb1bd75e0e1c6674ab0d9
Radin <60866293+RK129@users.noreply.github.com>
2024-04-13 16:47:44 -0700

    rounds code

--- backend/main.py
+++ backend/main.py
@@ -45,9 +45,7 @@ def signup():
 @app.route("/profile")
 def profile():
     return "Profile"
-@app.route("/gamewindow")
-def game_window():
-    return send_from_directory("../frontend", "gamewindow.html")
+
 
 
 # SocketIO events
