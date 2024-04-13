71c4a38a1275e13eb4a9eb7a58f458ab1a375ab7
siIverfish <ivan.g.zimmerman@gmail.com>
2024-04-13 12:16:55 -0700

    data load

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "hi!!!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run("0.0.0.0")
+    app.run("0.0.0.0", debug=True)
